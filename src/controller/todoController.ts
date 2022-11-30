c87c03f5606373cb21b12d98f13eb9e21fe9483c
Ndifreke Etukudo <ndifreke.etukudo@decagon.dev>
2022-11-30 18:40:39 +0100

    update and delete route setup

--- src/controller/todoController.ts
+++ src/controller/todoController.ts
@@ -69,3 +69,47 @@ export const createTodo = async (req: Request, res: Response) => {
         })
     }
 }
+export const updateTodo = async (req:Request, res:Response) => {
+    try{
+        const id = req.params.id;
+ const {description, status} = req.body;
+ const updatetodo = await Todo.findOne({"_id": id});
+ if(updatetodo){
+    const todo = await Todo.updateOne({"_id": id},{
+        description, status
+    })
+    return res.status(200).json({
+        status: 'updated successfully',
+        data: todo
+    })
+    }
+    return res.status(400).json({
+        message: "unidentified data"
+     }) 
+}catch(err){
+        return res.status(500).json({
+            message: 'internal server error',
+            routes: 'todo/update-todo/:id'
+         })  
+    }
+}
+export const removeTodo = async (req:Request, res:Response) => {
+    try{
+        const id = req.params.id;
+        
+        const deletetodo = await Todo.findOneAndDelete({"_id": id})
+
+        return res.status(200).json({
+           message: "deleted successfully"
+        })
+    
+        return res.status(400).json({
+            message: "unidentified data"
+         }) 
+    }catch(err){
+        return res.status(500).json({
+            message: 'internal server error',
+            routes: 'todo/remove-todo/:id'
+         })
+    }
+}
